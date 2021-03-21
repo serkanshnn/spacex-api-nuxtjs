faddd4f4b1d0e0f7ce74169433472f08319f800c
Serkan Şahin <serkan.sahin@epigra.com>
2021-03-21 16:47:19 +0300

    Add card list

---
+++ components/SpaceCraftCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex-1 bg-white flex flex-col justify-between">
+    <div class="flex-1">
+      <div class="flex-shrink-0">
+        <img class="h-48 w-full object-cover" src="https://media-rd.s3.amazonaws.com/embedded_image/2018/02/Blue%20Origin.png" alt="">
+      </div>
+      <a href="#" class="block p-4">
+        <h2 class="text-xl font-semibold text-gray-900 hover:underline">
+          {{ data.capsule_serial }}
+        </h2>
+        <p class="text-xs">
+          {{ launchDateTime }}
+        </p>
+        <span class="inline-flex items-center px-3 py-0.5 mt-3 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
+          {{ data.status }}
+        </span>
+        <p class="mt-3 text-base text-gray-500">
+          {{ data.details }}
+        </p>
+        <div class="mt-3">
+          <h3 class="text-md text-gray-900 font-semibold">Missions:</h3>
+          <ul class="list-decimal pl-5">
+            <li v-for="mission in data.missions" class="text-xs">
+              {{mission.name}} - {{mission.flight}}
+            </li>
+          </ul>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: ['data'],
+  setup(props) {
+    return { props }
+  },
+  computed: {
+    launchDateTime(props) {
+      let date = props.data.original_launch;
+      return moment(date).format('DD.MM.YYYY HH:mm');//props.data.original_launch;
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        spacex
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="relative bg-gray-50">
+    <div class="relative max-w-7xl mx-auto">
+      <div class="max-w-lg mx-auto grid gap-5 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 lg:max-w-none pt-4 pb-4">
+        <div class="flex flex-col rounded-lg shadow-lg overflow-hidden" v-for="data in results">
+          <space-craft-card :data="data"></space-craft-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { ref } from "@nuxtjs/composition-api";
+import usePromise from "@/plugins/use-promise";
+import axios from "axios";
+import SpaceCraftCard from "~/components/SpaceCraftCard";
+
+export default {
+  components: {
+    SpaceCraftCard
+  },
+  setup() {
+    let results = ref(null);
+    const getSpaceCrafts = usePromise(() =>
+      axios.get("https://api.spacexdata.com/v3/capsules"));
+    getSpaceCrafts.createPromise();
+    results = getSpaceCrafts.results;
+    return {results};
+  }
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
